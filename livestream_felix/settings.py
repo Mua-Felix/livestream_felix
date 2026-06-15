87ef4195138a25351e72ee3094ec2c17cb42b4b4
Mua-Felix <muafelix79@gmail.com>
2026-06-15 11:39:44 +0100

    fix ALLOWED HOSTS

--- livestream_felix/settings.py
+++ livestream_felix/settings.py
@@ -11,9 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key-change-this')
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
+ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
